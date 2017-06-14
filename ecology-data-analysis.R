3541f8bfa4333a2a8c1b1e0acdebb32fdea5fa14
ab604 <ab604@soton.ac.uk>
2017-06-14 17:15:44 +0100

    Update R lesson

--- ecology-data-analysis.R
+++ ecology-data-analysis.R
@@ -18,7 +18,6 @@
 # Load the tidyverse set of packages -------------------------------------------
 
 library(tidyverse)
-library(forcats)
 
 # Download the and import the data ---------------------------------------------
 
@@ -42,7 +41,8 @@ if (!file.exists(path)) { download.file(url,path) }
 # We'll assign the data to a R object called 'surveys'. 
 # read_csv creates a 'tibble' a modified form of data frame with enhanced
 # printing and checking capabilites.
-surveys <- read_csv('novice/R-ecology-lesson/data/portal_data_joined.csv')
+#surveys <- read_csv('novice/R-ecology-lesson/data/portal_data_joined.csv')
+surveys <- read_csv('data/portal_data_joined.csv')
 
 # Inspect the data with 'glimpse' ----------------------------------------------
 
@@ -91,23 +91,49 @@ exp_plots <- c("Control",
 # OX 	Onychomys sp. 	Rodent 	Grasshopper mouse
 
 # List of species_id
-rodents <- c("DM","DO","DS","PP","PF","PE","PM","RM","OX")
+#rodents <- c("DM","DO","DS","PP","PF","PE","PM","RM","OX")
+rodents <- c("DM","DO","DS","PP","PF","PE","PM","RM")
+
+# Control Plots: 2,8,12,22
+# Treatment Plots: 3,15,19,21
+plots <- c(8,11,12,14,3,15,19,21)
 
 # Let's get filter the data we need:
+# surveys_sub <- surveys %>%
+#   filter(species_id != "",            # remove missing species_id
+#          !is.na(weight),              # remove missing weight
+#          !is.na(hindfoot_length),     # remove missing hindfoot_length
+#          sex != "",                   # remove missing sex
+#          year <= 1991,                # Subset time period 1977 to 1991
+#          plot_id %in% plots,    # Subset the plots used in the paper
+#          species_id %in% rodents) %>%
+#   mutate(Date = dmy(sprintf('%02d%02d%04d', day, month, year))) %>%
+#   filter(Date >= as.Date("1977-09-01") & Date <= as.Date("1990-12-31")) %>%
+#   mutate(Quarter = lubridate::quarter(Date,with_year = TRUE))
+
+# We get a warning but, we've filtered them out
 surveys_sub <- surveys %>% 
-  filter(species_id != "",            # remove missing species_id
-         !is.na(weight),              # remove missing weight
-         !is.na(hindfoot_length),     # remove missing hindfoot_length
-         sex != "",                   # remove missing sex
-         year <= 1991,                # Subset time period 1977 to 1991
-         plot_type %in% exp_plots,    # Subset the plots used in the paper
-         species_id %in% rodents)
-  
+  # Add a date columns
+  mutate(Date = dmy(sprintf('%02d%02d%04d', day, month, year))) %>%
+  # Drop unwanted columns
+  select(-sex,-hindfoot_length,-weight) %>% 
+  # Filter time period in paper
+  filter(plot_id %in% plots, # Subset the plots
+         species_id %in% rodents, # Subset the species of interest
+         Date >= as.Date("1977-09-01") & Date <= as.Date("1990-12-31")) %>% 
+  # Add the quartley period
+  mutate(Quarter = lubridate::quarter(Date,with_year = TRUE))
+
+# Check whether there are any NAs
+which(is.na(surveys_sub$Date))
 
 glimpse(surveys_sub)
 
-# Q: How many of each type genus?
-surveys_sub %>% group_by(genus,species,plot_type) %>% count(species)
+# Q: How many of each species by genus?
+surveys_sub %>% group_by(genus) %>% count(species)
+
+# Check our selected plots
+distinct(surveys_sub,plot_id,plot_type)
 
 # Factors ----------------------------------------------------------------------
 
@@ -115,20 +141,30 @@ surveys_sub %>% group_by(genus,species,plot_type) %>% count(species)
 # Then we can group them accordingly.
 
 # Check levels
-levels(surveys_sub$species)
+levels(surveys_sub$species_id)
 
 # We have three groups we want: the Kangeroo Rats, the smaller granivores,
 # and the grasshopper mouse.
+
+# Kangeroo Rats:
+# DM 	Dipodomys merriami 	Rodent 	Merriam's kangaroo rat
+# DO 	Dipodomys ordii 	Rodent 	Ord's kangaroo rat
+# DS 	Dipodomys spectabilis 	Rodent 	Banner-tailed kangaroo rat
+
+# Granivores:
+# PP 	Chaetodipus penicillatus 	Rodent 	Desert pocket mouse
+# PF 	Perognathus flavus 	Rodent 	Silky pocket mouse
+# PE 	Peromyscus eremicus 	Rodent 	Cactus mouse
+# PM 	Peromyscus maniculatus 	Rodent 	Deer Mouse
+# RM 	Reithrodontomys megalotis 	Rodent 	Western harvest mouse
+
 surveys_sub <- surveys_sub %>% 
-  mutate(genus = fct_collapse(genus,
-                                "Kangeroo Rat" = "Dipodomys",
-                                "Granivore" = c("Perognathus",
-                                                "Peromyscus",
-                                                "Chaetodipus",
-                                                "Reithrodontomys"),
-                                "Grasshopper mouse"= "Onychomys")) %>% 
-  mutate(plot_type = as.factor(plot_type)) %>% 
-  mutate(species = as.factor(species))
+  mutate(species_name = fct_collapse(species_id,
+                                "Kangeroo Rat" = c("DM","DO","DS"),
+                                "Granivore" = c("PP","PF","PE","PM","RM")))#, 
+                                #"Grasshopper mouse"= "OX")) 
+
+  #mutate(species = as.factor(species))
 
 # Check the new levels
 surveys_sub %>% group_by(genus,species,plot_type) %>% count(species)
@@ -139,10 +175,28 @@ surveys_sub %>% group_by(genus,plot_type) %>% count(genus)
 
 # Let's aggregate the time data to every three months and calculate the number
 # of caputures
-by_month <- group_by(surveys_sub,year,month,genus,plot_type) #%>% count(genus)
-surveys_sum <- summarise(by_month,count = n())
+by_month <- surveys_sub %>% 
+  group_by(species_name,Date,plot_type) %>% 
+  summarise(captures = n()) 
 
-ggplot(data = surveys_sum,aes(x=year,y=count,colour=plot_type)) + geom_line() + facet_wrap(~ genus)
+by_quarter <- surveys_sub %>% 
+  group_by(species_name,Quarter,plot_type) %>% 
+  summarise(captures = n()/3) 
+
+
+ggplot(data = by_quarter,aes(x=Quarter,y=captures,colour=plot_type)) + 
+  geom_line() +
+  geom_point() + 
+  facet_grid(~ species_name) +
+  scale_x_continuous(breaks = seq(1977,1991,1))
+
+ggplot(data = by_month,aes(x=Date,y=captures,colour=plot_type)) + geom_line() +
+  geom_point() + 
+  facet_grid(~ species_name) 
+
+ggplot(data = by_quarter,aes(x=Quarter,y=captures,colour=genus)) + geom_line() +
+  geom_point() + 
+  facet_grid(plot_type ~ genus)
 
 by_species <- group_by(surveys_sub,species,plot_type) %>% 
   filter(genus == "Granivore")
